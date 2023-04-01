d4b3da57cf657bfcd37b396b21b2f277ec116958
Matthew Smart <matthewsmart13@gmail.com>
2023-04-01 09:10:27 -0500

    A way to import and store the gmae images

---
+++ src/components/cardData.js
@@ -0,0 +1,65 @@
+import donpatch0 from "../img/donpatch0.jpg";
+import donpatch1 from "../img/donpatch1.png";
+import donpatch2 from "../img/donpatch2.jpg";
+import donpatch3 from "../img/donpatch3.jpg";
+import donpatch4 from "../img/donpatch4.jpg";
+import donpatch5 from "../img/donpatch5.png";
+import donpatch6 from "../img/donpatch6.jpg";
+import donpatch7 from "../img/donpatch7.jpg";
+import donpatch8 from "../img/donpatch8.jpg";
+import donpatch9 from "../img/donpatch9.jpeg";
+import donpatch10 from "../img/donpatch10.jpeg";
+import donpatch11 from "../img/donpatch11.jpeg";
+
+const cardList = [
+	{
+		id: 0,
+		img: donpatch0,
+	},
+	{
+		id: 1,
+		img: donpatch1,
+	},
+	{
+		id: 2,
+		img: donpatch2,
+	},
+	{
+		id: 3,
+		img: donpatch3,
+	},
+	{
+		id: 4,
+		img: donpatch4,
+	},
+	{
+		id: 5,
+		img: donpatch5,
+	},
+	{
+		id: 6,
+		img: donpatch6,
+	},
+	{
+		id: 7,
+		img: donpatch7,
+	},
+	{
+		id: 8,
+		img: donpatch8,
+	},
+	{
+		id: 9,
+		img: donpatch9,
+	},
+	{
+		id: 10,
+		img: donpatch10,
+	},
+	{
+		id: 11,
+		img: donpatch11,
+	},
+];
+
+export default cardList;
