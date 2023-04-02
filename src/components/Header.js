61c9ad7fb8ad6c5782db692cbe312c67080a13b7
Matthew Smart <matthewsmart13@gmail.com>
2023-04-01 23:38:44 -0500

    Created a basic header and footer for page

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import bobobo from "../img/BoBoBo-Bo_Bo-BoBo_logo.webp";
+
+const Header = () => {
+	return (
+		<header className="header">
+			<img src={bobobo} alt="BoBoBo logo" />
+			<h2>The Many Faces of Don Patch Memory Card Game</h2>
+		</header>
+	);
+};
+
+export default Header;
