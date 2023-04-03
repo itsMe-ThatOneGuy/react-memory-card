2312ea4a5c73d1ba45d1a1ced5a32041789cf477
Matthew Smart <matthewsmart13@gmail.com>
2023-04-02 21:42:18 -0500

    Updated imports and added Game comp to the render

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import "./App.css";
 import Header from "./components/Header";
-import Scoreboard from "./components/Scoreboard";
-import Gameboard from "./components/Gameboard";
+import Game from "./components/Game";
 import Footer from "./components/Footer";
 
 function App() {
 	return (
 		<div className="App">
 			<Header />
-			<Scoreboard />
-			<Gameboard />
+			<Game />
 			<Footer />
 		</div>
 	);
