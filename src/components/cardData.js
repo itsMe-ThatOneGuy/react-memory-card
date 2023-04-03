68237d320d1c6d16506ccbffb1a08134461a43e2
Matthew Smart <matthewsmart13@gmail.com>
2023-04-03 11:50:55 -0500

    Refactored cards array to use map for adding each card object

--- src/components/cardData.js
+++ src/components/cardData.js
@@ -11,67 +11,26 @@ import donpatch9 from "../img/donpatch9.jpeg";
 import donpatch10 from "../img/donpatch10.jpeg";
 import donpatch11 from "../img/donpatch11.jpeg";
 
-const cards = [
-	{
-		id: 0,
-		img: donpatch0,
-		clicked: false,
-	},
-	{
-		id: 1,
-		img: donpatch1,
-		clicked: false,
-	},
-	{
-		id: 2,
-		img: donpatch2,
-		clicked: false,
-	},
-	{
-		id: 3,
-		img: donpatch3,
-		clicked: false,
-	},
-	{
-		id: 4,
-		img: donpatch4,
-		clicked: false,
-	},
-	{
-		id: 5,
-		img: donpatch5,
-		clicked: false,
-	},
-	{
-		id: 6,
-		img: donpatch6,
-		clicked: false,
-	},
-	{
-		id: 7,
-		img: donpatch7,
-		clicked: false,
-	},
-	{
-		id: 8,
-		img: donpatch8,
-		clicked: false,
-	},
-	{
-		id: 9,
-		img: donpatch9,
-		clicked: false,
-	},
-	{
-		id: 10,
-		img: donpatch10,
-		clicked: false,
-	},
-	{
-		id: 11,
-		img: donpatch11,
-		clicked: false,
-	},
+const images = [
+	donpatch0,
+	donpatch1,
+	donpatch2,
+	donpatch3,
+	donpatch4,
+	donpatch5,
+	donpatch6,
+	donpatch7,
+	donpatch8,
+	donpatch9,
+	donpatch10,
+	donpatch11,
 ];
+const cards = images.map((img, i) => {
+	return {
+		id: i,
+		img: img,
+		clicked: false,
+	};
+});
 
 export default cards;
