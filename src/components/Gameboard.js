0de67a2db020418b34e9a78189890ef811ae8dc9
Matthew Smart <matthewsmart13@gmail.com>
2023-04-02 21:44:22 -0500

    Changed shuffle function. Made function to handle image clicks

--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -1,28 +1,53 @@
 import React from "react";
 import "./Gameboard.css";
-import Cards from "./Card";
+import Card from "./Card";
 import cardList from "./cardData";
 
 class Gameboard extends React.Component {
 	constructor(props) {
 		super(props);
-
 		this.state = {
 			cardList: cardList,
-			shouldShuffle: false,
+			shouldShuffle: true,
 		};
 
 		this.shuffleCards = this.shuffleCards.bind(this);
+		this.selectCard = this.selectCard.bind(this);
+		this.test = this.test.bind(this);
+	}
+
+	createCardList() {
+		const newList = [...this.state.cardList];
+	}
+
+	getSelectedCard(e) {
+		const selectCard = e.target.id;
+		const cardIndex = this.state.cardList.findIndex(
+			(card) => selectCard === `Image${card.id}`
+		);
+		return cardIndex;
+	}
+
+	selectCard(e) {
+		const newList = [...this.state.cardList];
+		newList[this.getSelectedCard(e)].cliked = true;
+		this.setState({
+			cardList: newList,
+			shouldShuffle: true,
+		});
 	}
 
 	shuffleCards() {
-		const shuffled = this.state.cardList
-			.map((value) => ({ value, sort: Math.random() }))
-			.sort((a, b) => a.sort - b.sort)
-			.map(({ value }) => value);
+		const shuffled = [...this.state.cardList].sort(() => Math.random() - 0.5);
 		this.setState({
 			cardList: shuffled,
+			shouldShuffle: false,
 		});
+		console.log(shuffled);
+	}
+
+	test() {
+		this.shuffleCards();
 	}
 
 	componentDidMount() {
@@ -32,12 +57,15 @@ class Gameboard extends React.Component {
 	render() {
 		return (
 			<div className="gameboard">
+				<button onClick={this.test}>Test</button>
 				{this.state.cardList.map((e) => {
-					return <Cards key={e.id} info={e} />;
+					return <Card key={e.id} info={e} selectCard={this.selectCard} />;
 				})}
 			</div>
 		);
 	}
+
+	componentDidUpdate() {}
 }
 
 export default Gameboard;
